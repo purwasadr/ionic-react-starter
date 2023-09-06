4a52d9aa98e67e3d1e919988679ebce205968c22
Purwa Sadr <purwasadr@gmail.com>
2023-09-06 13:40:44 +0700

    init again

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.react.starter',
   appName: 'ionic-react-starter',
   webDir: 'dist',
   server: {
